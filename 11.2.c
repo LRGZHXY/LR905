a370fbddfab1edbdc3938705eb5d9a753486604b
LRGZHXY <3399075402@qq.com>
2023-11-02 21:36:20 +0800

    222

---
+++ 11.2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void func(void) {
+    short a = -2;
+    unsigned int b = 1;
+    b += a;
+    int c = -1;
+    unsigned short d = c * 256;
+    c <<= 4;
+    int e = 2;
+    e = ~e | 6;
+    d = (d & 0xff) + 0x2022;
+    printf("a=0x%hx\tb=0x%x\td=0x%hx\te=0x%x\n", a, b, d, e);
+    printf("c=Ox%hhx\t\n", (signed char)c);
+    
+}
