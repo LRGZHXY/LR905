94c14bfec48fc4bee9799ebf739c992e3e23363f
lr <3399075402@qq.com>
2023-11-20 20:16:35 +0800

    12

--- cpp/1+2.c
+++ cpp/1+2.c
@@ -20,37 +20,37 @@ int main()
  }
 
 
-#include <stdio.h>
-#include <math.h>
-int main()
-{
-    float a,b,c,p,s;
-    scanf("%f %f %f",&a,&b,&c);
-    if(a+b>c&&b+c>a&&a+c>b)
-    {
-        p=(a+b+c)/2;
-        s=sqrt(p*(p-a)*(p-b)*(p-c));
-        printf("三角形面积s=%.2f",s);
-    }
-    else
-    {
-        printf("三角形不存在");
-    }
-    return 0;
-}
+// #include <stdio.h>
+// #include <math.h>
+// int main()
+// {
+//     float a,b,c,p,s;
+//     scanf("%f %f %f",&a,&b,&c);
+//     if(a+b>c&&b+c>a&&a+c>b)
+//     {
+//         p=(a+b+c)/2;
+//         s=sqrt(p*(p-a)*(p-b)*(p-c));
+//         printf("三角形面积s=%.2f",s);
+//     }
+//     else
+//     {
+//         printf("三角形不存在");
+//     }
+//     return 0;
+// }
 
 
-#include <stdio.h>
-#define a 3.14
-int main()
-{
-    float r,s,v,b=1.25;
-    scanf("%f",&r);
-    s=a*r*r;
-    v=b*a*r*r*r;
-    printf("s=%f,v=%f",s,v);
-    return 0;
-}
+// #include <stdio.h>
+// #define a 3.14
+// int main()
+// {
+//     float r,s,v,b=1.25;
+//     scanf("%f",&r);
+//     s=a*r*r;
+//     v=b*a*r*r*r;
+//     printf("s=%f,v=%f",s,v);
+//     return 0;
+// }
 
  
  #include <stdio.h>
