6d15a7d72ce78bce27bd945ab99c20213dd6f81d
lr <3399075402@qq.com>
2023-11-20 20:14:49 +0800

    33

---
+++ cpp/3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int main()
+{
+    int ch;
+    scanf("%d",&ch);
+    printf("%c",ch);
+    return 0;
+}
+
+
+#include <stdio.h>
+int main()
+{
+    float n;
+    printf("Enter a floating-point value: ");
+    scanf("%f",&n);
+    printf("fixed-point notation:%f\n",n);
+    printf("exponential notation:%e\n",n);
+    printf("p notation:%a",n);
+    return 0;
+}
+
+
+#include <stdio.h>
+int main()
+{
+    int n,pint,ounce,bigspoon,teaspoon;
+    printf("Please you enter a number of cups: ");
+    scanf("%d",&n);
+    pint=n/2;
+    ounce=n*8;
+    bigspoon=n*16;
+    teaspoon=n*48;
+    printf("Pint:%d\n",pint);
+    printf("Ounce:%d\n",ounce);
+    printf("Bigspoon:%d\n",bigspoon);
+    printf("Teaspoon:%d\n",teaspoon);
+    return 0;
+}
+
+
