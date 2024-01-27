f4e87c1f8f02ad7fa744ab368d88070b3e33b73f
lr <3399075402@qq.com>
2024-01-27 20:18:16 +0800

    88888

--- 905428/l.c
+++ 905428/l.c
@@ -488,3 +488,47 @@ int** generateMatrix(int n, int* returnSize, int** returnColumnSizes){
 //         if n % 2 != 0 :			# n为奇数时，填充中心点
 //             nums[mid][mid] = count 
 //         return nums
+
+
+// var generateMatrix = function(n) {
+//     let startX = startY = 0;   // 起始位置
+//     let loop = Math.floor(n/2);   // 旋转圈数
+//     let mid = Math.floor(n/2);    // 中间位置
+//     let offset = 1;    // 控制每一层填充元素个数
+//     let count = 1;     // 更新填充数字
+//     let res = new Array(n).fill(0).map(() => new Array(n).fill(0));
+
+//     while (loop--) {
+//         let row = startX, col = startY;
+//         // 上行从左到右（左闭右开）
+//         for (; col < n - offset; col++) {
+//             res[row][col] = count++;
+//         }
+//         // 右列从上到下（左闭右开）
+//         for (; row < n - offset; row++) {
+//             res[row][col] = count++;
+//         }
+//         // 下行从右到左（左闭右开）
+//         for (; col > startY; col--) {
+//             res[row][col] = count++;
+//         }
+//         // 左列做下到上（左闭右开）
+//         for (; row > startX; row--) {
+//             res[row][col] = count++;
+//         }
+
+//         // 更新起始位置
+//         startX++;
+//         startY++;
+
+//         // 更新offset
+//         offset += 1;
+//     }
+//     // 如果n为奇数的话，需要单独给矩阵最中间的位置赋值
+//     if (n % 2 === 1) {
+//         res[mid][mid] = count;
+//     }
+//     return res;
+// };
+
+  
