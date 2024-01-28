d5e3390471f67403a69acb61fd1dedad46edf450
lr <3399075402@qq.com>
2024-01-28 22:33:28 +0800

    888888

--- 905428/l.c
+++ 905428/l.c
@@ -531,4 +531,10 @@ int** generateMatrix(int n, int* returnSize, int** returnColumnSizes){
 //     return res;
 // };
 
+// 单链表
+struct ListNode {
+    int val;  // 节点上存储的元素
+    ListNode *next;  // 指向下一个节点的指针
+    ListNode(int x) : val(x), next(NULL) {}  // 节点的构造函数
+};
   
