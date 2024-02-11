fe867ca2a0404a8b4a7b04e05db966fa906cb2b2
lr <3399075402@qq.com>
2024-02-11 20:34:43 +0800

    0

--- 905428/l.c
+++ 905428/l.c
@@ -697,3 +697,99 @@ int** generateMatrix(int n, int* returnSize, int** returnColumnSizes){
 //     // 返回头结点
 //     return head;
 // }
+// typedef struct {
+//     int val;
+//     struct MyLinkedList* next;
+// } MyLinkedList;
+
+
+// MyLinkedList* myLinkedListCreate() {
+//     MyLinkedList* head = (MyLinkedList *)malloc(sizeof (MyLinkedList));
+//     head->next = NULL;
+//     return head;
+// }
+
+// int myLinkedListGet(MyLinkedList* obj, int index) {
+//      MyLinkedList *cur = obj->next;
+//     for (int i = 0; cur != NULL; i++){
+//         if (i == index){
+//             return cur->val;
+//         }
+//         else{
+//             cur = cur->next;
+//         }
+//     }
+//     return -1;
+// }
+
+// void myLinkedListAddAtHead(MyLinkedList* obj, int val) {
+//     MyLinkedList *nhead = (MyLinkedList *)malloc(sizeof (MyLinkedList));
+//     nhead->val = val;
+//     nhead->next = obj->next;
+//     obj->next = nhead;
+
+// }
+
+// void myLinkedListAddAtTail(MyLinkedList* obj, int val) {
+//     MyLinkedList *cur = obj;
+//     while(cur->next != NULL){
+//         cur = cur->next;
+//         }
+//     MyLinkedList *ntail = (MyLinkedList *)malloc(sizeof (MyLinkedList));
+//     ntail->val = val;
+//     ntail->next = NULL;
+//     cur->next = ntail;
+// }
+
+// void myLinkedListAddAtIndex(MyLinkedList* obj, int index, int val) {
+//     if (index == 0){
+//         myLinkedListAddAtHead(obj, val);
+//         return;
+//     }
+//     MyLinkedList *cur = obj->next;
+//     for (int i = 1 ;cur != NULL; i++){
+//         if (i == index){
+//             MyLinkedList* newnode = (MyLinkedList *)malloc(sizeof (MyLinkedList));
+//             newnode->val = val;
+//             newnode->next = cur->next;
+//             cur->next = newnode;
+//             return;
+//         }
+//         else{
+//             cur = cur->next;
+//         }
+//     }
+// }
+
+// void myLinkedListDeleteAtIndex(MyLinkedList* obj, int index) {
+//     if (index == 0){
+//         MyLinkedList *tmp = obj->next;
+//         if (tmp != NULL){
+//             obj->next = tmp->next;
+//             free(tmp);     
+//         }
+//         return;
+//     }
+//     MyLinkedList *cur = obj->next;
+//     for (int i = 1 ;cur != NULL && cur->next != NULL; i++){
+//         if (i == index){
+//             MyLinkedList *tmp = cur->next;
+//             if (tmp != NULL) {
+//                 cur->next = tmp->next;
+//                 free(tmp);
+//             }
+//             return;
+//         }
+//         else{
+//             cur = cur->next;
+//         }
+//     }
+// }
+
+// void myLinkedListFree(MyLinkedList* obj) {
+//     while(obj != NULL){
+//         MyLinkedList *tmp = obj;
+//         obj = obj->next;
+//         free(tmp);
+//     }
+// }
