df080b9045547515b5ea98df4d1758e17c5462ae
lr <3399075402@qq.com>
2024-02-14 22:11:26 +0800

    11111111

--- 905428/l.c
+++ 905428/l.c
@@ -811,3 +811,21 @@ int** generateMatrix(int n, int* returnSize, int** returnColumnSizes){
 //     newHead->next = head;
 //     return newHead;
 // }
+// #include <stdio.h>
+// void one_three(void)
+// {
+//     printf("one\n");
+//     two();
+//     printf("three\n");
+// }
+// void two()
+// {
+//     printf("two\n");
+// }
+// int main()
+// {
+//     printf("starting now:\n");
+//     one_three();
+//     printf("done!");
+//     return 0;
+// }
